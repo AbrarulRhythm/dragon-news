65849b98582c38b5a2d167a2e8dd2ef935e38772
Abrarul Rhythm <[....email>
2025-10-18 01:21:28 +0600

    added navbar component

---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { NavLink } from 'react-router';
+import user from '../../assets/user.png';
+
+const NavBar = () => {
+    return (
+        <div className='container'>
+            <div className='flex justify-between items-center'>
+                <div></div>
+                <div className='nav'>
+                    <ul className='text-dark-3 font-medium flex items-center space-x-6'>
+                        <li>
+                            <NavLink to='/'>Home</NavLink>
+                        </li>
+                        <li>
+                            <NavLink to='/about'>About</NavLink>
+                        </li>
+                        <li>
+                            <NavLink to='/career'>Career</NavLink>
+                        </li>
+                    </ul>
+                </div>
+                <div className='flex items-center space-x-2'>
+                    <img className='w-[40px]' src={user} alt='User Image' />
+                    <button className='bg-dark-2 text-white font-medium py-3 px-10 cursor-pointer'>Login</button>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default NavBar;
--- src/layouts/HomeLayout/HomeLayout.jsx
+++ src/layouts/HomeLayout/HomeLayout.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Outlet } from 'react-router';
 import Header from '../../components/Header/Header';
 import LatestNews from '../../components/LatestNews/LatestNews';
+import NavBar from '../../components/NavBar/NavBar';
 
 const HomeLayout = () => {
     return (
@@ -11,6 +12,9 @@ const HomeLayout = () => {
                 <section className='mt-7'>
                     <LatestNews></LatestNews>
                 </section>
+                <nav className='mt-5'>
+                    <NavBar></NavBar>
+                </nav>
             </header>
             <main>
                 <section className='left_nav'></section>
